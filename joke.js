1b4d38f10eef41a4f827d7d6a42bb4936388753e
Hadi Shah <hadishah.work@gmail.com>
2025-02-09 22:10:25 +0530

    :fire: API key changed

--- joke.js
+++ joke.js
@@ -1,50 +1,22 @@
 const apiBody = document.querySelector(".api_body"); 
-    // const apiUrl= "https://jsonplaceholder.typicode.com/posts/1";
-    const apiUrl= "https://icanhazdadjoke.com";
+const apiUrl = "https://official-joke-api.appspot.com/random_joke";
 
-    // Using Promises...
-    const fetchData=()=>{
-        const response = fetch(apiUrl,{
-            headers:{//dont know wh but necessary
-                Accept: "application/json",
-            },
-        })
-        .then((res)=>{
-            // console.log(res);
-            return res.json();//to convert the doc/obj into json
-        })
-        .then((data)=>{
-            console.log(data.joke);
-            apiBody.innerHTML = data.joke;
-        })
-        .catch((error)=>{
-            apiBody.innerHTML = "API is not working:(";
-            console.log("failed to fetch...try again with different API url:)");
-        })
-    }
-    
-    // Using async await ---> no option to display error in asyncawait that's why we use try and catch in async&await ==>nothing but known as error handling... 
-    // async function fetchData() {//
-    // const fetchData = async() => {//using FAT ARROW func.
-    //     try{
-    //         const res = await fetch(apiUrl,{
-    //             headers:{
-    //                 Accept:"application/json", 
-    //             },
-    //         });
-    //         const data= await res.json();
-    //         console.log(data.joke);
-    //         apiBody.innerHTML = data.joke;
-    //     }
-    //     catch(error){
-    //         apiBody.innerHTML = "API is not working:(";
-    //         console.log("failed to fetch...try again with different API url:)");
-            
-    //     }
-        
-    // }
+const fetchData = () => {
+    fetch(apiUrl, {
+        headers: {
+            Accept: "application/json",
+        },
+    })
+    .then((res) => res.json()) 
+    .then((data) => {
+        console.log(data.setup);
+        console.log(data.punchline);
+        apiBody.innerHTML = `${data.setup} - ${data.punchline}`;
+    })
+    .catch((error) => {
+        apiBody.innerHTML = "API is not working:(";
+        console.log("Failed to fetch... Try again with a different API URL.");
+    });
+}
 
-
-    // fetchData();
-
-    document.getElementById("fetchJoke").addEventListener("click",fetchData); 
+document.getElementById("fetchJoke").addEventListener("click", fetchData);
